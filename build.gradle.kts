ead6d6b94ff9def32d19921de38559519a48caae
noakafka <winds0515@naver.com>
2024-08-21 14:28:35 +0900

    fix: 불필요한 implemetation 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,9 +46,6 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
-	testImplementation("org.mockito:mockito-inline:5.0.0")
-
 }
 
 kotlin {
