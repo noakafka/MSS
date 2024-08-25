f5a43fcd3bb3fb36f8766ca6ff395ef2dd987ab4
noakafka <winds0515@naver.com>
2024-08-25 20:30:00 +0900

    fix: brand, category entity name에 unique 속성 추가

--- src/main/kotlin/com/mss/assignment/domain/category/Category.kt
+++ src/main/kotlin/com/mss/assignment/domain/category/Category.kt
@@ -1,6 +1,7 @@
 package com.mss.assignment.domain.category
 
 import com.mss.assignment.domain.BaseEntity
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -14,6 +15,7 @@ class Category(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0
 
+    @Column(unique = true, nullable = false)
     var name: String = name
         private set
 }
